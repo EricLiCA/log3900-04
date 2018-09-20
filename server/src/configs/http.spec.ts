d8614524440910c49b580a5dcd20aaa9fa9636a8
Eric <sida.li@polymtl.ca>
2018-09-20 19:36:12 -0400

    Added mocha test framework to NodeJS server

---
+++ server/src/configs/http.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { SERVER_PORT } from './http';
+
+describe('Http', () => {
+    describe('SERVER_PORT', () => {
+        it('should be 3000', () => {
+            expect(SERVER_PORT).to.equal('3000');
+        });
+    });
+});
