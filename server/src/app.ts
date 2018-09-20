43aa993f428c540c02282fbe546264651cf93792
Eric <sida.li@polymtl.ca>
2018-09-20 15:39:50 -0400

    Wrote initial boilerplate for NodeJS server

---
+++ server/src/app.ts
@@ -0,0 +1,103 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+
+import * as helloWorldRoute from './routes/hello-world';
+
+export class Application {
+
+    public app: express.Application;
+
+    /**
+     * Bootstrap the application.
+     *
+     * @class Server
+     * @method bootstrap
+     * @static
+     * @return {ng.auto.IInjectorService} Returns the newly created injector for this this.app.
+     */
+    public static bootstrap(): Application {
+        return new Application();
+    }
+
+    /**
+     * Constructor.
+     *
+     * @class Server
+     * @constructor
+     */
+    constructor() {
+
+        // Application instantiation
+        this.app = express();
+
+        // configure this.application
+        this.config();
+
+        // configure routes
+        this.routes();
+    }
+
+    /**
+     * The config function.
+     *
+     * @class Server
+     * @method config
+     */
+    private config() {
+        // Middlewares configuration
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(express.static(path.join(__dirname, '../static')));
+        this.app.use(cors());
+    }
+
+    /**
+     * The routes function.
+     *
+     * @class Server
+     * @method routes
+     */
+    public routes() {
+        let router: express.Router;
+        router = express.Router();
+
+        // create routes
+        const helloWorld: helloWorldRoute.HelloWorld = new helloWorldRoute.HelloWorld();
+
+        // hello world path
+        router.get('/hello/', helloWorld.hello.bind(helloWorld.hello));
+
+        // use router middleware
+        this.app.use('/api', router);
+
+        // error management
+        this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            const err = new Error('Not Found');
+            next(err);
+        });
+
+        // development error handler
+        // will print stacktrace
+        if (this.app.get('env') === 'development') {
+            this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+                res.status(err.status || 500);
+                res.send({
+                    message: err.message,
+                    error: err
+                });
+            });
+        }
+
+        // production error handler
+        // no stacktraces leaked to user (in production env only)
+        this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.status(err.status || 500);
+            res.send({
+                message: err.message,
+                error: {}
+            });
+        });
+    }
+}
