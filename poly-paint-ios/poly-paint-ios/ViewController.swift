85727ecd3ae669e83422472de496e21edf935ad7
Eric <sida.li@polymtl.ca>
2018-09-22 11:58:50 -0400

    iOS initial setup

---
+++ poly-paint-ios/poly-paint-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poly-paint-ios
+//
+//  Created by Eric Sida Li on 2018-09-22.
+//  Copyright © 2018 PolyAcme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
