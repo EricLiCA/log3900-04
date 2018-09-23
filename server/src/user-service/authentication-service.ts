d555c6ffced71393889c3333fc8fcdb4410df5dc
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-09-23 00:12:05 -0400

    Added password generation and validation methods to the authentication service

--- server/src/user-service/authentication-service.ts
+++ server/src/user-service/authentication-service.ts
@@ -1,3 +1,4 @@
+import { compare, hash, genSalt } from 'bcrypt-nodejs';
 export class AuthenticationService {
 
     private static _instance: AuthenticationService;
@@ -11,5 +12,40 @@ export class AuthenticationService {
         }
 
         return AuthenticationService._instance;
+    }
+
+    public hashPassword(password: string) : Promise<string> {
+        return new Promise<string> ((resolve, reject) => {
+            genSalt(10, (error: Error, salt: string) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                hash(password, salt, (err: Error, hash: string) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+                    resolve(hash);
+                });
+            });
+        });
+    }
+
+    public validateCredentials(email: string, password: string) : Promise<boolean> {
+        const hash = /* MUST FETCH FROM THE DATABASE */ "";
+        
+        return new Promise<boolean> ((resolve, reject) => {
+            compare(password, hash, (error: Error, result: boolean) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(result);
+            });
+        });
     }
 }
