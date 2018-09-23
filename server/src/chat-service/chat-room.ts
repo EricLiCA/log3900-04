e24655e6ec4b0295d8d5a3b181c3f9e2862eb842
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-09-23 13:30:24 -0400

    Refactored the ChatRoom implementation from Sockets to Connections

--- server/src/chat-service/chat-room.ts
+++ server/src/chat-service/chat-room.ts
@@ -1,6 +1,8 @@
+import { Connection } from "./connection";
+
 export class ChatRoom {
     
-    private _participants: SocketIO.Socket[];
+    private _participants: Connection[];
 
     constructor(private _id: String) {
         this._participants = [];
@@ -10,16 +12,16 @@ export class ChatRoom {
         return this._id;
     };
 
-    public get participants(): SocketIO.Socket[] {
+    public get participants(): Connection[] {
         return this._participants;
     };
 
     public add(socket: SocketIO.Socket): void {
-        this._participants.forEach((participant: SocketIO.Socket) => {
-            participant.emit('message', `${socket.handshake.address} has joined the chat room`);
+        this._participants.forEach((participant: Connection) => {
+            participant.socket.emit('message', `${socket.handshake.address} has joined the chat room`);
         });
 
-        this._participants.push(socket);
+        this._participants.push(new Connection(socket));
         socket.emit('message', 'You have joined the Chat Room!');
 
         socket.on('chat', (args: any[]) => this.onChat(socket, String(args[0])));
@@ -27,18 +29,18 @@ export class ChatRoom {
     }
 
     private onChat(socket: SocketIO.Socket, message: String) {
-        this._participants.forEach((participant: SocketIO.Socket) => {
-            if (participant !== socket) {
-                participant.emit('chat', message);
+        this._participants.forEach((participant: Connection) => {
+            if (participant.socket !== socket) {
+                participant.socket.emit('chat', message);
             }
         });
     }
 
     private onDisconnect(socket: SocketIO.Socket) {
-        delete this.participants.splice(this.participants.indexOf(socket), 1)[0];
+        delete this.participants.splice(this.participants.findIndex(participant => participant.socket === socket), 1)[0];
         
-        this.participants.forEach((participant: SocketIO.Socket) => {
-            participant.emit('message', `${socket.handshake.address} has left the chat room`)
+        this.participants.forEach((participant: Connection) => {
+            participant.socket.emit('message', `${socket.handshake.address} has left the chat room`)
         });
     }
 }
