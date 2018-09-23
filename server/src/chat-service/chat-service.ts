26d04471d9ffc12fc34d2bc10f2cd0e8ca329bb0
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-09-23 00:17:45 -0400

    Hooked the chat rooms to the chat service

--- server/src/chat-service/chat-service.ts
+++ server/src/chat-service/chat-service.ts
@@ -1,11 +1,15 @@
 import { SocketServer } from '../socket-server';
+import { ChatRoom } from './chat-room';
 
 export class ChatService {
     private static chatService: ChatService;
     private io: SocketIO.Server;
 
+    private rooms: Map<String, ChatRoom>
+
     private constructor() {
         this.io = SocketServer.getInstance();
+        this.rooms = new Map();
     }
 
     public static getInstance(): ChatService {
@@ -21,7 +25,25 @@ export class ChatService {
 
     private listenForConnections(): void {
         this.io.on('connection', (socket: SocketIO.Socket) => {
-            socket.emit('chat', 'You are now connected to the chat service!');
+            socket.on('joinRoom', args => this.joinRoom(socket, args));
+            console.log(`New socket connection from ${socket.handshake.address}`);
         });
+        
+    }
+
+    private joinRoom(socket: SocketIO.Socket, args: any[]): void {
+        if (args.length === 0) {
+            socket.emit('error', 'Your request must contain at least two parameters to connect to a room');
+            return;
+        }
+
+        let room = this.rooms.get(args[0]);
+        if (!room) {
+            room = new ChatRoom(args[0]);
+            this.rooms.set(args[0], room);
+        }
+        
+        socket.removeAllListeners('joinRoom');
+        room.add(socket);
     }
 }
