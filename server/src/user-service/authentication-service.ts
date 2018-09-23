f3af49aa200f53642acf0f100c7db31ff4b5ff94
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-09-23 00:12:36 -0400

    Added token generation and validation to authentication service

--- server/src/user-service/authentication-service.ts
+++ server/src/user-service/authentication-service.ts
@@ -1,9 +1,13 @@
 import { compare, hash, genSalt } from 'bcrypt-nodejs';
+import { sign, verify, JsonWebTokenError, NotBeforeError, TokenExpiredError } from 'jsonwebtoken';
+
 export class AuthenticationService {
 
     private static _instance: AuthenticationService;
+    private secret: string;
 
     constructor() {
+        this.secret = this.generateRandomSecret(32);
     }
 
     public static get instance(): AuthenticationService {
@@ -12,6 +16,14 @@ export class AuthenticationService {
         }
 
         return AuthenticationService._instance;
+    }
+
+    private generateRandomSecret(length: number): string {
+        let secret = '';
+        for (let i = 0; i < length; i++) {
+            secret += String.fromCharCode(48 + Math.floor(Math.random() * 77));
+        }
+        return secret;
     }
 
     public hashPassword(password: string) : Promise<string> {
@@ -45,6 +57,23 @@ export class AuthenticationService {
                 }
 
                 resolve(result);
+            });
+        });
+    }
+
+    public generateJsonwebtoken(email: string): string {
+        return sign( { email }, this.secret, { expiresIn: '6h' } );
+    }
+
+    public validateJsonwebtoken(token: string) : Promise<string> {
+        return new Promise<string> ((resolve, reject) => {
+            verify(token, this.secret, (error: JsonWebTokenError | NotBeforeError | TokenExpiredError, decoded: object | string) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(decoded['user']);
             });
         });
     }
