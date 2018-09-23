76a8e7a25d763c956c51f0260ad49b3ede27347e
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-09-23 12:11:10 -0400

    Added Utils to hold generic functions

---
+++ server/src/utils/Utils.spec.ts
@@ -0,0 +1,6 @@
+
+    /*
+     * Because source-map-support uses Math.random() and source-map-support
+     * is used by sinon, Math.random() can't be stubed to test the secret
+     * generation.
+     */
