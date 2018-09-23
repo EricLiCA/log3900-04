93c4ed02e6cc5d7ac6236db2021fa17d03555daf
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-09-23 12:32:39 -0400

    Added tests for function generateRandomSecret in Utils

--- server/src/utils/Utils.ts
+++ server/src/utils/Utils.ts
@@ -3,7 +3,7 @@ export namespace Utils {
     export function generateRandomSecret(length: number): string {
         let secret = '';
         for (let i = 0; i < length; i++) {
-            secret += String.fromCharCode(48 + Math.floor(Math.random() * 77));
+            secret += String.fromCharCode(48 + Math.floor(Math.random() * 75));
         }
         return secret;
     }
