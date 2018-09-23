2f6db0e3755335580ac0cd08cfdabc34bc0e114a
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-09-23 09:46:03 -0400

    Added tests for class User

---
+++ server/src/user-service/user.spec.ts
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import { User } from "./user";
+import { spy } from 'sinon';
+
+describe("User", () => {
+
+    let randomStub = spy( Math, 'random');
+    
+    beforeEach(() => {
+        randomStub.resetHistory();
+    });
+
+    after(() => {
+        randomStub.restore();
+    })
+
+    describe("if the user is authenticated", () => {
+        
+        it("should not instanciate if email is not specified", () => {
+            expect( () => {
+                new User(true);
+            }).to.throw( Error );
+        });
+        
+        it("should instanciate correctly if email is not specified", () => {
+            
+            let user = new User(true, 'email@email.com');
+            expect(user.email).to.equal('email@email.com');
+            // expect(user.id)   /* WILL BE CHECKED ONCE DATABASE FETCHING IS IMPLEMENTED */
+            // expect(user.name) /* WILL BE CHECKED ONCE DATABASE FETCHING IS IMPLEMENTED */
+        });
+    });
+    
+    describe("if the user is not authenticated", () => {
+
+        it("should instanciate correctly", () => {
+            let user = new User(false);
+            let randomNumber = randomStub.firstCall.returnValue;
+            expect(user.name).to.equal(`Anonymous #${Math.ceil(randomNumber * 10000)}`);
+        });
+    });
+});
