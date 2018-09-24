234cfe30857c2b4a0275a0dfe38e11366495080b
Eric <sida.li@polymtl.ca>
2018-09-24 08:20:33 -0400

    Chat

--- poly-paint-ios/poly-paint-ios/LoginViewController.swift
+++ poly-paint-ios/poly-paint-ios/LoginViewController.swift
@@ -8,26 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var username: UITextField!
-    
-    @IBAction func joinChat(_ sender: Any) {
-        if username.text != "" {
-            performSegue(withIdentifier: "toChatRoomSegue", sender: self)
-        }
-    }
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    @IBOutlet weak var messageTableView: UITableView!
+    var messagesArray = [String]()
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var dockViewHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.messageTableView.delegate = self
+        self.messageTableView.dataSource = self
+        
+        // Set as delegate for the textfield
+        self.messageTextField.delegate = self
+        
+        // Add some sample data so that we can see something
+        messagesArray.append("Test 1")
+        messagesArray.append("Test 2")
+        messagesArray.append("Test 3")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hideKeyboard()
+    }
+    
+    func hideKeyboard() {
+        self.view.endEditing(true)
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.1, animations: {
+            self.dockViewHeightConstraint.constant = 60
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
 
-
+    @IBAction func sendButtonTap(_ sender: UIButton) {
+        hideKeyboard()
+    }
+    
+    // MARK: TextField Delegate Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hideKeyboard()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.1, animations: {
+            self.dockViewHeightConstraint.constant = 400
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        hideKeyboard()
+    }
+    
+    // MARK: TableView Delegate Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messagesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create a table cell
+        guard let cell = messageTableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath) as? UITableViewCell else {
+            fatalError("Could not dequeue UITableViewCell")
+        }
+        
+        // Customize the cell
+        cell.textLabel?.text = messagesArray[indexPath.row]
+        
+        // Return the cell
+        return cell
+    }
 }
 
