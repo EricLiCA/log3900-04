36e6294be0c884a68b8bc98347014db189780128
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-10-07 15:35:38 -0400

    Logout clears userdefaults.

--- poly-paint-ios/poly-paint-ios/MainMenuViewController.swift
+++ poly-paint-ios/poly-paint-ios/MainMenuViewController.swift
@@ -18,6 +18,9 @@ class MainMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        let newBackButton = UIBarButtonItem(title: "Logout", style: UIBarButtonItemStyle.plain, target: self, action: #selector(MainMenuViewController.back(sender:)))
+        self.navigationItem.leftBarButtonItem = newBackButton
 
         // Do any additional setup after loading the view.
     }
@@ -27,6 +30,19 @@ class MainMenuViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @objc func back(sender: UIBarButtonItem) {
+        // Go back to the previous ViewController and clear UserDefaults
+        self.resetDefaults()
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    func resetDefaults() {
+        let defaults = UserDefaults.standard
+        let dictionary = defaults.dictionaryRepresentation()
+        dictionary.keys.forEach { key in
+            defaults.removeObject(forKey: key)
+        }
+    }
 
     /*
     // MARK: - Navigation
