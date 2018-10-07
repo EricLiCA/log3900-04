8f097d845d4de2b14f82a189cc9756c4e6708db1
Eric <sida.li@polymtl.ca>
2018-10-07 12:03:28 -0400

    Removed dead code

--- server/src/chat-service/chat-service.ts
+++ server/src/chat-service/chat-service.ts
@@ -1,19 +1,11 @@
 import { SocketServer } from '../socket-server';
-// import { AuthenticationService } from "../user-service/authentication-service";
-// import { ChatRoom } from "./chat-room";
-// import { Connection } from "./connection";
 
 export class ChatService {
     private static chatService: ChatService;
     private usernames: Map<string, string>;
     private connectedUsers: Set<string>;
 
-    /* Keep it simple (not this release)
-    private rooms: Map<string, ChatRoom>;
-    */
-
     private constructor() {
-        // this.rooms = new Map();
         // Reserve key usernames
         this.usernames = new Map();
         this.connectedUsers = new Set();
@@ -34,15 +26,6 @@ export class ChatService {
 
     private listenForConnections(): void {
         SocketServer.instance.on('connection', (socket: SocketIO.Socket) => {
-            /* scope creep
-            const connection = new Connection(socket);
-            */
-            /* Not for this release: scope creep (Keep it simple; KISS principle)
-            socket.on("login", (...args: any[]) => this.login(connection, args));
-            */
-            /* No rooms for this release neither, just main room required (Keep it simple)
-            socket.on("joinRoom", (...args: any[]) => this.joinRoom(connection, args));
-            */
             console.log(`New socket connection with id ${socket.id}`);
             socket.on('setUsername', (username: string) => {
                 console.log(`${socket.id} wants to set username as ${username}`);
@@ -74,44 +57,5 @@ export class ChatService {
                 }
             });
         });
-
     }
-
-    /* scope creep
-    private login(connection: Connection, args: any[]): void {
-        if (args.length < 2) {
-            connection.socket.emit("error", "Your request must contain the email and the password");
-            return;
-        }
-
-        AuthenticationService.instance.validateCredentials(args[0], args[1]).then((valid) => {
-            if (!valid) {
-                connection.socket.emit("err", "User or Password is not valid");
-                return;
-            }
-
-            connection.connect(args[0]);
-            connection.socket.emit("logged-in", AuthenticationService.instance.generateJsonwebtoken(args[0]));
-        }, (rejectReason) => {
-            throw new Error(rejectReason);
-        }).catch((error: Error) => {
-            connection.socket.emit("err", error.message);
-        });
-    }
-
-    private joinRoom(connection: Connection, args: any[]): void {
-        if (args.length === 0) {
-            connection.socket.emit("error", "Your request must contain at least two parameters to connect to a room");
-            return;
-        }
-
-        let room = this.rooms.get(args[0]);
-        if (!room) {
-            room = new ChatRoom(args[0]);
-            this.rooms.set(args[0], room);
-        }
-
-        connection.socket.removeAllListeners("joinRoom");
-        room.add(connection);
-    }*/
 }
--- server/src/routes/sessions.ts
+++ server/src/routes/sessions.ts
@@ -69,4 +69,53 @@ export class SessionsRoute {
                 res.sendStatus(400); // Bad request
             });
     }
+
+    public async logout(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
+        const db = await PostgresDatabase.getInstance();
+        db.query('SELECT * FROM Users WHERE "Username" = $1', [req.body.username]).then((query) => {
+            if (query.rowCount > 0) {
+                const result = query.rows[0];
+                if (req.body.password === result.Password) {
+                    const redisClient = RedisService.getInstance();
+                    redisClient.hget('authTokens', result.Id, (redisErr, cachedToken) => {
+                        if (cachedToken !== undefined) {
+                            res.send({
+                                id: result.Id,
+                                token: cachedToken,
+                            });
+                        } else {
+                            db.query(
+                                `INSERT INTO Sessions("userid")
+                                VALUES($1)
+                                ON CONFLICT ("userid")
+                                DO UPDATE SET "userid" = excluded.userid
+                                RETURNING *`,
+                                [result.Id],
+                            )
+                                .then((queryResponse) => {
+                                    if (queryResponse.rowCount > 0) {
+                                        const sessionResult = queryResponse.rows[0];
+
+                                        res.send({
+                                            id: sessionResult.userid,
+                                            token: sessionResult.token,
+                                        });
+                                    }
+                                })
+                                .catch((err) => {
+                                    res.sendStatus(400);
+                                });
+                        }
+                    });
+                } else {
+                    res.sendStatus(403);
+                }
+            } else {
+                res.sendStatus(404);
+            }
+        })
+            .catch((err) => {
+                res.sendStatus(400); // Bad request
+            });
+    }
 }
