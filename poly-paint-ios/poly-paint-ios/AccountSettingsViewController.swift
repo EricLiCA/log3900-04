9851c9c7b4c87d61e3d008f1aad23deb9ca571eb
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-10-09 13:52:36 -0400

    User is able to change username

--- poly-paint-ios/poly-paint-ios/AccountSettingsViewController.swift
+++ poly-paint-ios/poly-paint-ios/AccountSettingsViewController.swift
@@ -14,7 +14,6 @@ class AccountSettingsViewController: UIViewController {
     @IBOutlet weak var oldPasswordTextField: UITextField!
     @IBOutlet weak var newPasswordTextField: UITextField!
     @IBOutlet weak var confirmNewPasswordTextField: UITextField!
-
     @IBOutlet weak var usernameAlreadyExistsLabel: UILabel!
     
     override func viewDidLoad() {
@@ -29,16 +28,54 @@ class AccountSettingsViewController: UIViewController {
     }
     
     @IBAction func changeUsernameTapped(_ sender: UIButton) {
+        let username = self.newUsernameTextField.text
         
-        // send request to server
-        
-        // If success (username doesn't exist), update userDefault
-        
-        // If failure (username exists), show failure label
-        self.usernameAlreadyExistsLabel.isHidden = false
+        if username != "" && username != UserDefaults.standard.string(forKey: "username") {
+            self.changeUsername(username: username!)
+        }
     }
     
     @IBAction func changePasswordTapped(_ sender: UIButton) {
+    }
+    
+    func changeUsername(username: String) {
+        let urlString = "http://localhost:3000/v1/users/" + UserDefaults.standard.string(forKey: "id")!
+        let url = URL(string: urlString)
+        let session = URLSession.shared
+        var request = URLRequest(url: url!)
+        request.httpMethod = "PUT"
+        
+        // Setting data to send
+        let paramToSend: [String: Any] = ["username": username as Any, "token": UserDefaults.standard.string(forKey: "token") as Any]
+        let jsonData = try? JSONSerialization.data(withJSONObject: paramToSend, options: .prettyPrinted)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            let httpResponse = response as? HTTPURLResponse
+            print(httpResponse?.statusCode as Any)
+            guard let data = data, error == nil else {
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if (responseJSON as? [String: Any]) != nil {
+                DispatchQueue.main.async {
+                    self.changeUsernameDone(username: username)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.usernameAlreadyExistsLabel.isHidden = false
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func changeUsernameDone(username: String) {
+        UserDefaults.standard.set(username, forKey: "username")
+        self.usernameAlreadyExistsLabel.isHidden = true
+        self.newUsernameTextField.text = ""
     }
     
     /*
