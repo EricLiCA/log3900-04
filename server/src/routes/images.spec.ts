4321906929d455766b80d7b23f03fcc03206d85e
Aymen <aymenamous@gmail.com>
2018-10-09 22:07:21 -0400

    added comment to images api test

--- server/src/routes/images.spec.ts
+++ server/src/routes/images.spec.ts
@@ -1,5 +1,7 @@
 const IMAGES_URL: String = "http://localhost:3000/images/";
 const TEST: string = "TEST"
+// TODO: we should mock the database in order to make a proper test
+
 
 describe("ImagesRoute", () => {
     const chai = require('chai');
@@ -7,19 +9,6 @@ describe("ImagesRoute", () => {
     chai.use(chaiHttp);
 
     it("should add a new image", (done: MochaDone) => {
-        const newImage = {
-            ownerId: TEST,
-            title: TEST,
-            protectionLevel: TEST,
-            password: TEST,
-            thumbnailUrl: TEST,
-            fullImageUrl: TEST,
-        }
-        // tslint:disable-next-line:no-any
-        chai.request(IMAGES_URL).post("post").send({ newImage }).end((err: boolean, res: any) => {
-            console.log(res);
-            chai.expect(res).to.equal("");
-        });
-        // tslint:disable-next-line:no-magic-numbers
+        
     }).timeout(15000);
 });
