b696fafe945cc7f9d9ae9879698a1cd54af8870e
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-10-11 12:52:17 -0400

    Add button desable when clicked

--- poly-paint-ios/poly-paint-ios/FriendsManagementViewController.swift
+++ poly-paint-ios/poly-paint-ios/FriendsManagementViewController.swift
@@ -13,9 +13,16 @@ struct Headline {
 }
 
 class HeadlineTableViewCell: UITableViewCell {
-    @IBOutlet weak var usernameLabel: UILabel!
     
+    @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var addAsFriendButton: UIButton!
+    
+    @IBAction func addAssFriendTapped(_ sender: UIButton) {
+        print("ADD user: " + usernameLabel.text!)
+        addAsFriendButton.isEnabled = false
+        addAsFriendButton.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.098/255, alpha: 0.22)
+    
+    }
 }
 
 
