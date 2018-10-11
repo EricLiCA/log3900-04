50797d9ea9bcbe61db41fea2f2c868fa7eb54ad9
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-11 13:03:12 -0400

    Added basic Chat Room class

---
+++ windows-client/PolyPaint/Modeles/ChatRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.Modeles
+{
+    class ChatRoom
+    {
+        public string Name { get; set; }
+        public ObservableCollection<User> Users { get; set; }
+
+        public ChatRoom(string name)
+        {
+            this.Name = name;
+            this.Users = new ObservableCollection<User>();
+            this.Users.Add(new User("Francis", "anything", true));
+            this.Users.Add(new User("Joshua", "anything", false));
+            this.Users.Add(new User("Hana", "anything", true));
+        }
+    }
+}
