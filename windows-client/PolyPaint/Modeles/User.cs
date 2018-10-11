38bb2edcf5a815b498239fb540015ad5a0441d2b
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-11 13:02:56 -0400

    Added Basic User class

---
+++ windows-client/PolyPaint/Modeles/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace PolyPaint.Modeles
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public BitmapImage ProfileImage { get; set; }
+        public bool Friend;
+
+        public User(string name, string profileImageUrl, bool friend)
+        {
+            this.Username = name;
+            this.Friend = friend;
+            this.ProfileImage = new BitmapImage(new System.Uri("https://firebase.google.com/_static/images/firebase/touchicon-180.png"));
+        }
+    }
+}
