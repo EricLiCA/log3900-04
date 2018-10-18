6b4974627f47106e048f5f0eaa85dd6691e53a65
Eric <sida.li@polymtl.ca>
2018-10-18 12:03:40 -0400

    Users.get(id)

--- server/src/models/User.spec.ts
+++ server/src/models/User.spec.ts
@@ -3,18 +3,35 @@ import { User } from './User';
 import { expect } from 'chai';
 
 describe('User', () => {
-    it('should return all users', (done) => {
-        User.getAll().then((results) => {
-            expect(results).length.to.be.greaterThan(0);
-            expect(results[0].Id).to.not.be.empty;
-            expect(results[0].Password).to.not.be.empty;
-            expect(results[0].ProfileImage).to.not.be.empty;
-            expect(results[0].UserLevel).to.not.be.empty;
-            expect(results[0].Username).to.not.be.empty;
-            done();
-        })
-            .catch((err) => {
-                expect(false).to.be.true;
-            });
+    describe('getAll', () => {
+        it('should return all users', (done) => {
+            User.getAll().then((results) => {
+                expect(results).length.to.be.greaterThan(0);
+                expect(results[0].Id).to.not.be.empty;
+                expect(results[0].Password).to.not.be.empty;
+                expect(results[0].ProfileImage).to.not.be.empty;
+                expect(results[0].UserLevel).to.not.be.empty;
+                expect(results[0].Username).to.not.be.empty;
+                done();
+            })
+                .catch((err) => {
+                    expect(false).to.be.true;
+                });
+        });
+    });
+
+    describe('get (by id)', () => {
+        it('should return the user "polypaint"', (done) => {
+            User.get('556722f1-1051-438c-9046-2d220c273f91').then((result) => {
+                console.log(result);
+                expect(result.Id).to.equal('556722f1-1051-438c-9046-2d220c273f91');
+                expect(result.UserLevel).to.equal('admin');
+                expect(result.Username).to.equal('polypaintpro');
+                done();
+            })
+                .catch((err) => {
+                    expect(false);
+                });
+        });
     });
 });
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -19,6 +19,23 @@ export class User {
         }
     }
 
+    public static async get(id: string): Promise<User> {
+        const db = await PostgresDatabase.getInstance();
+        const queryResponse = await db.query('SELECT * FROM Users WHERE "Id" = $1', [id]);
+        if (queryResponse.rowCount > 0) {
+            const row = queryResponse.rows[0];
+            return Promise.resolve(new User(
+                row.Id,
+                row.Username,
+                row.Password,
+                row.UserLevel,
+                row.ProfileImage,
+            ));
+        } else {
+            return Promise.resolve(undefined);
+        }
+    }
+
     public Id: string;
     public Username: string;
     public Password: string;
