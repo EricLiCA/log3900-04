b1b267d5f97288b5e5950649f10a9ee71a18ea2f
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-18 14:39:00 -0400

    Added user class on the server

---
+++ server/src/connected-users-service.ts/user.ts
@@ -0,0 +1,13 @@
+import { Socket } from "socket.io";
+
+export class User {
+    public anonymous: boolean;
+    public socket: Socket;
+    public name: string;
+
+    constructor(anonymous: boolean, socket: Socket, name: string) {
+        this.anonymous = anonymous;
+        this.socket = socket;
+        this.name = name;
+    }
+}
