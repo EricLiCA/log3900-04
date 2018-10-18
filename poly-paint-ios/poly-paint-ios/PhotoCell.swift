e9310b1b7aa1905c93180a92d0474798dd6104ae
Jean Paul Cech <jean-paul.cech@polymtl.ca>
2018-10-18 11:44:48 -0400

    added class representing gallery image

--- poly-paint-ios/poly-paint-ios/PhotoCell.swift
+++ poly-paint-ios/poly-paint-ios/PhotoCell.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2018 PolyAcme. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
