58b7da80f7379ca2f83bec6da0b87f888a9451e4
Eric <sida.li@polymtl.ca>
2018-10-18 12:07:24 -0400

    [refactor] GET /users/:id

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -20,22 +20,18 @@ export class UsersRoute {
     }
 
     public async get(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
-        const db = await PostgresDatabase.getInstance();
-        db.query('SELECT * FROM Users WHERE "Id" = $1', [req.params.id]).then((query) => {
-            if (query.rowCount > 0) {
-                const result = query.rows[0];
-                res.send({
-                    id: result.Id,
-                    username: result.Username,
-                    userLevel: result.UserLevel,
-                });
-            } else {
+        User.get(req.params.id).then((user) => {
+            if (user === undefined) {
                 res.sendStatus(404);
+            } else {
+                res.send({
+                    id: user.Id,
+                    username: user.Username,
+                    userLevel: user.UserLevel,
+                    profileImage: user.ProfileImage,
+                });
             }
-        })
-            .catch((err) => {
-                res.sendStatus(400); // Bad request
-            });
+        });
     }
 
     public async post(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
