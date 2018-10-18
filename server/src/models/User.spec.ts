3930891dd32a2ffae4e557bfa7be17bd6027b9e9
Eric <sida.li@polymtl.ca>
2018-10-18 11:41:51 -0400

    Fixed mocha tests not exiting...

--- server/src/models/User.spec.ts
+++ server/src/models/User.spec.ts
@@ -3,18 +3,18 @@ import { User } from './User';
 import { expect } from 'chai';
 
 describe('User', () => {
-    describe('getAll()', () => {
-        it('should return all users', (done) => {
-            User.getAll().then((results) => {
-                console.log(results);
-                expect(results).length.to.be.greaterThan(0);
-                expect(results[0].Id).to.not.be.empty;
-                expect(results[0].Password).to.not.be.empty;
-                expect(results[0].ProfileImage).to.not.be.empty;
-                expect(results[0].UserLevel).to.not.be.empty;
-                expect(results[0].Username).to.not.be.empty;
-                done();
+    it('should return all users', (done) => {
+        User.getAll().then((results) => {
+            expect(results).length.to.be.greaterThan(0);
+            expect(results[0].Id).to.not.be.empty;
+            expect(results[0].Password).to.not.be.empty;
+            expect(results[0].ProfileImage).to.not.be.empty;
+            expect(results[0].UserLevel).to.not.be.empty;
+            expect(results[0].Username).to.not.be.empty;
+            done();
+        })
+            .catch((err) => {
+                expect(false).to.be.true;
             });
-        });
     });
 });
