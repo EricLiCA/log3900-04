20159bda81a6742b01548e85e2bc9fca3f690056
Eric <sida.li@polymtl.ca>
2018-10-18 12:22:15 -0400

    User create and delete tested

--- server/src/models/User.spec.ts
+++ server/src/models/User.spec.ts
@@ -3,6 +3,29 @@ import { User } from './User';
 import { expect } from 'chai';
 
 describe('User', () => {
+    describe('create and delete', () => {
+        const username = '123testUser';
+        const password = '321testPassword';
+        let id: string;
+        it('should create a user with the username and password provided', (done) => {
+            User.create(username, password).then((result) => {
+                expect(result.Username).to.equal(username);
+                expect(result.Password).to.equal(password);
+                id = result.Id;
+                done();
+            });
+        });
+
+        it('should delete the previously created user', (done) => {
+            User.delete(id).then((result) => {
+                expect(result.Username).to.equal(username);
+                expect(result.Password).to.equal(password);
+                expect(result.Id).to.equal(id);
+                done();
+            });
+        });
+    });
+
     describe('getAll', () => {
         it('should return all users', (done) => {
             User.getAll().then((results) => {
@@ -23,7 +46,6 @@ describe('User', () => {
     describe('get (by id)', () => {
         it('should return the user "polypaint"', (done) => {
             User.get('556722f1-1051-438c-9046-2d220c273f91').then((result) => {
-                console.log(result);
                 expect(result.Id).to.equal('556722f1-1051-438c-9046-2d220c273f91');
                 expect(result.UserLevel).to.equal('admin');
                 expect(result.Username).to.equal('polypaintpro');
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -36,6 +36,46 @@ export class User {
         }
     }
 
+    public static async create(username: string, password: string): Promise<User> {
+        const db = await PostgresDatabase.getInstance();
+        const queryResponse = await db.query(
+            'INSERT INTO Users("Username", "Password") VALUES($1, $2) RETURNING *',
+            [username, password],
+        );
+        if (queryResponse.rowCount > 0) {
+            const row = queryResponse.rows[0];
+            return Promise.resolve(new User(
+                row.Id,
+                row.Username,
+                row.Password,
+                row.UserLevel,
+                row.ProfileImage,
+            ));
+        } else {
+            return Promise.resolve(undefined);
+        }
+    }
+
+    public static async delete(id: string): Promise<User> {
+        const db = await PostgresDatabase.getInstance();
+        const queryResponse = await db.query(
+            'DELETE FROM Users WHERE "Id" = $1 RETURNING *',
+            [id],
+        );
+        if (queryResponse.rowCount > 0) {
+            const row = queryResponse.rows[0];
+            return Promise.resolve(new User(
+                row.Id,
+                row.Username,
+                row.Password,
+                row.UserLevel,
+                row.ProfileImage,
+            ));
+        } else {
+            return Promise.resolve(undefined);
+        }
+    }
+
     public Id: string;
     public Username: string;
     public Password: string;
