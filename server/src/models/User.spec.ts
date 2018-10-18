e8ea1b4dc31759b238c931df9f4a432d3907a1c3
Eric <sida.li@polymtl.ca>
2018-10-18 12:57:13 -0400

    Created CRUD for User model

--- server/src/models/User.spec.ts
+++ server/src/models/User.spec.ts
@@ -3,57 +3,84 @@ import { User } from './User';
 import { expect } from 'chai';
 
 describe('User', () => {
-    describe('create and delete', () => {
-        const username = '123testUser';
-        const password = '321testPassword';
-        let id: string;
-        it('should create a user with the username and password provided', (done) => {
-            User.create(username, password).then((result) => {
-                expect(result.Username).to.equal(username);
-                expect(result.Password).to.equal(password);
-                id = result.Id;
-                done();
-            });
-        });
+    let username: string = '123testUser';
+    let password: string = '321testPassword';
+    let id: string;
+    let userLevel: string;
+    let profileImage: string;
 
-        it('should delete the previously created user', (done) => {
-            User.delete(id).then((result) => {
-                expect(result.Username).to.equal(username);
-                expect(result.Password).to.equal(password);
-                expect(result.Id).to.equal(id);
-                done();
-            });
+    it('should create a user with the username and password provided', (done) => {
+        User.create(username, password).then((result) => {
+            expect(result.Username).to.equal(username);
+            expect(result.Password).to.equal(password);
+            expect(result.ProfileImage).to.equal(
+                // tslint:disable-next-line:max-line-length
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjqmTWoUhezVh6rd7F0DYqkpqDGAwbYoC_hEfi0nphYL1h08gCkA'
+            );
+            id = result.Id;
+            userLevel = result.UserLevel;
+            profileImage = result.ProfileImage;
+            done();
         });
     });
 
-    describe('getAll', () => {
-        it('should return all users', (done) => {
-            User.getAll().then((results) => {
-                expect(results).length.to.be.greaterThan(0);
-                expect(results[0].Id).to.not.be.empty;
-                expect(results[0].Password).to.not.be.empty;
-                expect(results[0].ProfileImage).to.not.be.empty;
-                expect(results[0].UserLevel).to.not.be.empty;
-                expect(results[0].Username).to.not.be.empty;
-                done();
-            })
-                .catch((err) => {
-                    expect(false).to.be.true;
-                });
+    it('should return the previously created user', (done) => {
+        User.get(id).then((result) => {
+            expect(result.Id).to.equal(id);
+            expect(result.UserLevel).to.equal(userLevel);
+            expect(result.Username).to.equal(username);
+            expect(result.ProfileImage).to.equal(profileImage);
+            done();
+        })
+            .catch((err) => {
+                expect(false);
+            });
+    });
+
+    it('should update the username of the previously created user', (done) => {
+        username = '456testUser';
+        User.update(id, username, undefined, undefined, undefined).then((result) => {
+            expect(result.Id).to.equal(id);
+            expect(result.Username).to.equal(username);
+            done();
         });
     });
 
-    describe('get (by id)', () => {
-        it('should return the user "polypaint"', (done) => {
-            User.get('556722f1-1051-438c-9046-2d220c273f91').then((result) => {
-                expect(result.Id).to.equal('556722f1-1051-438c-9046-2d220c273f91');
-                expect(result.UserLevel).to.equal('admin');
-                expect(result.Username).to.equal('polypaintpro');
-                done();
-            })
-                .catch((err) => {
-                    expect(false);
-                });
+    it('should update the password, userLevel, and profileImage of the user', (done) => {
+        password = '654testPassword';
+        userLevel = 'admin';
+        profileImage = 'http://placekitten.com/200/300';
+        User.update(id, undefined, password, userLevel, profileImage).then((result) => {
+            expect(result.Id).to.equal(id);
+            expect(result.UserLevel).to.equal(userLevel);
+            expect(result.Username).to.equal(username);
+            expect(result.ProfileImage).to.equal(profileImage);
+            done();
+        });
+    });
+
+    it('should return all users', (done) => {
+        User.getAll().then((results) => {
+            expect(results).length.to.be.greaterThan(0);
+            expect(results[0].Id).to.not.be.empty;
+            expect(results[0].Password).to.not.be.empty;
+            expect(results[0].ProfileImage).to.not.be.empty;
+            expect(results[0].UserLevel).to.not.be.empty;
+            expect(results[0].Username).to.not.be.empty;
+            done();
+        })
+            .catch((err) => {
+                expect(false).to.be.true;
+            });
+    });
+
+    it('should delete the previously created user', (done) => {
+        User.delete(id).then((result) => {
+            expect(result.Username).to.equal(username);
+            expect(result.Password).to.equal(password);
+            expect(result.Id).to.equal(id);
+            expect(result.ProfileImage).to.equal(profileImage);
+            done();
         });
     });
 });
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -56,6 +56,56 @@ export class User {
         }
     }
 
+    public static async update(
+        id: string,
+        username: string,
+        password: string,
+        userLevel: string,
+        profileImage: string,
+    ): Promise<User> {
+        let updates = [
+            ['Username', username],
+            ['Password', password],
+            ['UserLevel', userLevel],
+            ['ProfileImage', profileImage],
+        ];
+        updates = updates.filter((update) => update[1] !== undefined);
+        if (updates.length === 0) {
+            return Promise.resolve(undefined);
+        } else {
+            // Build the query : UPDATE Users SET col1 = val1, col2 = val2, ... WHERE Id = <id>;
+            let queryText = 'UPDATE Users SET ';
+            updates.forEach((update, i) => {
+                queryText += `"${update[0]}" = $${i + 1}`;
+                if (i !== updates.length - 1) {
+                    queryText += ',';
+                }
+            });
+            queryText += ` WHERE "Id" = $${updates.length + 1} RETURNING *`;
+
+            const preparedQuery = {
+                text: queryText,
+                values: updates.map((update) => update[1]).concat([id]),
+            };
+
+            // Query the database
+            const db = await PostgresDatabase.getInstance();
+            const queryResponse = await db.query(preparedQuery);
+            if (queryResponse.rowCount > 0) {
+                const row = queryResponse.rows[0];
+                return Promise.resolve(new User(
+                    row.Id,
+                    row.Username,
+                    row.Password,
+                    row.UserLevel,
+                    row.ProfileImage,
+                ));
+            } else {
+                return Promise.resolve(undefined);
+            }
+        }
+    }
+
     public static async delete(id: string): Promise<User> {
         const db = await PostgresDatabase.getInstance();
         const queryResponse = await db.query(
