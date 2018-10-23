154b5abf8a04185a58d5086f836d4fd63b251d01
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-10-23 17:52:13 -0400

    added s3 image uplaod service

---
+++ server/src/s3-service/s3-image-upload-service.ts
@@ -0,0 +1,10 @@
+import * as multer from 'multer';
+import * as multers3 from 'multer-s3';
+import * as aws from 'aws-sdk';
+
+export class S3ImageUploadService {
+    
+    private constructor() {
+        
+    }
+}
