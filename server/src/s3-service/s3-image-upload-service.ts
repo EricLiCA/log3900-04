7ca2a450569a74028789ef047eea857a1865bc3e
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-10-23 17:54:54 -0400

    added constructor

--- server/src/s3-service/s3-image-upload-service.ts
+++ server/src/s3-service/s3-image-upload-service.ts
@@ -4,7 +4,15 @@ import * as aws from 'aws-sdk';
 
 export class S3ImageUploadService {
     
+    public s3UploadServiceInstance: aws.S3;
+
     private constructor() {
-        
+        aws.config.update({
+            secretAccessKey: "fefe",
+            accessKeyId: "",
+            region: 'us-east-1'
+        })
+
+        this.s3UploadServiceInstance = new aws.S3();
     }
 }
