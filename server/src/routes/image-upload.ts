072676d5109650b17fe82c9c41c0317d10d83604
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-10-23 18:13:31 -0400

    added image upload route

---
+++ server/src/routes/image-upload.ts
@@ -0,0 +1,14 @@
+import * as express from 'express';
+import { S3ImageUploadService } from '../s3-service/s3-image-upload-service';
+
+
+export class S3ImageUploadRoute {
+
+  public async post(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
+
+    
+
+  }
+
+
+}
