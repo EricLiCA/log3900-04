7edf19349020ecc1179629afebf0763ac233bc12
Jean Paul Cech <jean-paul.cech@polymtl.ca>
2018-10-23 15:05:49 -0400

    added download of image from url

--- poly-paint-ios/poly-paint-ios/PublicPhotosViewController.swift
+++ poly-paint-ios/poly-paint-ios/PublicPhotosViewController.swift
@@ -15,61 +15,66 @@ final class PublicPhotosViewController: UICollectionViewController {
     fileprivate let reuseIdentifier = "PublicImageCell"
     fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0);
     fileprivate var searches = [Image]();
-    fileprivate let itemsPerRow: CGFLoat = 3 ;
-    
+    fileprivate let itemsPerRow: CGFloat = 3 ;
     var images:[Image]?
     
     func fetchPublicImages() {
-    guard let url = URL(string: "http://localhost:3000/images") else { return }
-    URLSession.shared.dataTask(with: url) { (data, response, error) in
-    
-    if error != nil {
-    print(error ?? "")
-    return
+        guard let url = URL(string: "http://localhost:3000/v1/images") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                print(error ?? "")
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                
+                self.images = [Image]()
+                
+                for dictionary in json as! [[String: AnyObject]] {
+                    
+                    let image = Image()
+                    image.id = dictionary["id"] as? String
+                    image.ownerId = dictionary["ownerId"] as? String
+                    image.title = dictionary["title"] as? String
+                    image.protectionLevel = dictionary["protectionLevel"] as? String
+                    image.password = dictionary["password"] as? String
+                    image.thumbnailUrl = dictionary["thumbnailUrl"] as? String
+                    image.fullImageUrl = dictionary["fullImageUrl"] as? String
+                    guard let url = image.getFullImageUrl(),
+                        let imageData = try? Data(contentsOf: url as URL) else {
+                            break
+                    }
+                    if let downloadedImage = UIImage(data: imageData){
+                        image.fullImage = downloadedImage
+                        self.images?.append(image)
+                    }
+                }
+                
+                DispatchQueue.main.async {
+                    self.collectionView?.reloadData()
+                }
+                
+                
+            } catch let jsonError {
+                print(jsonError)
+            }
+            
+            }.resume()
     }
     
-    do {
-    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
-    
-    self.images = [Image]()
-    
-    for dictionary in json as! [[String: AnyObject]] {
-    
-    let image = Image()
-        image.id = dictionary["id"] as? String
-        image.ownerId = dictionary["ownerId"] as? String
-        image.title = dictionary["title"] as? String
-        image.protectionLevel = dictionary["protectionLevel"] as? String
-        image.password = dictionary["password"] as? String
-        image.thumbnailUrl = dictionary["thumbnailUrl"] as? String
-        image.fullImageUrl = dictionary["fullImageUrl"] as? String
-    
-    self.images?.append(image)
+    override func viewDidLoad(){
+        fetchPublicImages()
     }
     
-    DispatchQueue.main.async {
-    self.collectionView?.reloadData()
-    }
-    
-    
-    } catch let jsonError {
-    print(jsonError)
-    }
-    
-    
-    
-    }.resume()
-    }
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images?.count
+        return images?.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.reuseIdentifier, for: indexPath) as! ImageCell
-        
-        cell.imageView.image = image?[indexPath.item].thumbnailUrl
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.reuseIdentifier, for: indexPath) as! PhotoCell
+        cell.imageView.image = images?[indexPath.item].fullImage
         return cell
     }
     
