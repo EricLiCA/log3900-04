76a5f3daf81ad4ee5c661933785f13667a8e482f
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-25 11:35:07 -0400

    Added Gallery base component

---
+++ web-client/src/gallery/gallery.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gallery-component',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+export class GalleryComponent {
+}
