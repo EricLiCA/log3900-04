663a6332d3d884fdd22686155232fef3810c0333
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-27 19:56:03 -0400

    Added the basic tool abstract class

---
+++ windows-client/PolyPaint/Modeles/Tools/Tool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.Modeles.Outils
+{
+    abstract class Tool
+    {
+        public string ToolName { get => this.GetToolName(); }
+        public string ToolImage { get => this.GetToolImage(); }
+        public string ToolTooltip { get => this.GetToolTooltip(); }
+
+        public abstract string GetToolName();
+        public abstract string GetToolImage();
+        public abstract string GetToolTooltip();
+
+        public abstract void OnMouseDown();
+        public abstract void OnDrag();
+        public abstract void OnMouseUp();
+    }
+}
