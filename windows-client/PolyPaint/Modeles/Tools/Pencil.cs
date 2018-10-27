e5a1b599d3dec6505d51a24757bfd6ace2a4d1a2
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-27 19:57:20 -0400

    Added Pencil Object

---
+++ windows-client/PolyPaint/Modeles/Tools/Pencil.cs
@@ -0,0 +1,42 @@
+﻿using PolyPaint.Modeles.Outils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.Modeles.Tools
+{
+    class Pencil : Tool
+    {
+        public override string GetToolImage()
+        {
+            return "/Resources/pencil-tool.png";
+        }
+
+        public override string GetToolName()
+        {
+            return "pencil";
+        }
+
+        public override string GetToolTooltip()
+        {
+            return "Pencil";
+        }
+
+        public override void OnDrag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnMouseDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnMouseUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
