ef280a34da6a2fe055d72dde8ce07d6fa4bad5b3
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-27 19:57:30 -0400

    Added Object Eraser Object

---
+++ windows-client/PolyPaint/Modeles/Tools/ObjectEraser.cs
@@ -0,0 +1,42 @@
+﻿using PolyPaint.Modeles.Outils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.Modeles.Tools
+{
+    class ObjectEraser : Tool
+    {
+        public override string GetToolImage()
+        {
+            return "/Resources/eraser_variant-tool.png";
+        }
+
+        public override string GetToolName()
+        {
+            return "object_eraser";
+        }
+
+        public override string GetToolTooltip()
+        {
+            return "Object Eraser";
+        }
+
+        public override void OnDrag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnMouseDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnMouseUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
