5c8fb403b65a0ba71476fce2ea8cc63367b2472f
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-27 19:57:43 -0400

    Added Segment Eraser Object

---
+++ windows-client/PolyPaint/Modeles/Tools/SegmentEraser.cs
@@ -0,0 +1,42 @@
+﻿using PolyPaint.Modeles.Outils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.Modeles.Tools
+{
+    class SegmentEraser : Tool
+    {
+        public override string GetToolImage()
+        {
+            return "/Resources/eraser-tool.png";
+        }
+
+        public override string GetToolName()
+        {
+            return "segment_eraser";
+        }
+
+        public override string GetToolTooltip()
+        {
+            return "Segment Eraser";
+        }
+
+        public override void OnDrag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnMouseDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnMouseUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
