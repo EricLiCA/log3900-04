2c8765c6f4f0aebb50f79bd4a7824579e4900ca3
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-29 08:37:59 -0400

    Added CustomStroke that extends Stroke

---
+++ windows-client/PolyPaint/Modeles/Strokes/CustomStroke.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Input;
+
+namespace PolyPaint.Modeles
+{
+    public abstract class CustomStroke : Stroke
+    {
+        public CustomStroke(StylusPointCollection pts) : base(pts)
+        {
+            
+        }
+
+        public abstract StrokeType getType();
+    }
+
+    public enum StrokeType
+    {
+        OBJECT, RESIZE_GUIDE, ANCHOR_POINT
+    }
+}
