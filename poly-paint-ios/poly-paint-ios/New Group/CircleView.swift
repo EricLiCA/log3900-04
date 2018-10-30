7b5049eb881ed57a035185bd50f6ba745b16b01e
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-10-30 15:56:21 -0400

    Added files for each shape

---
+++ poly-paint-ios/poly-paint-ios/New Group/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  poly-paint-ios
+//
+//  Created by Tomato on 2018-10-30.
+//  Copyright © 2018 PolyAcme. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ poly-paint-ios/poly-paint-ios/New Group/SquareView.swift
@@ -0,0 +1,21 @@
+//
+//  SquareView.swift
+//  poly-paint-ios
+//
+//  Created by Tomato on 2018-10-30.
+//  Copyright © 2018 PolyAcme. All rights reserved.
+//
+
+import UIKit
+
+class SquareView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
