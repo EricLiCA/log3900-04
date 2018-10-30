500d892ffa5a40c883bd414b040cc1ab87ea04d7
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-10-30 12:57:56 -0400

    Added Anchor Point class

---
+++ windows-client/PolyPaint/Modeles/Strokes/AnchorPoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace PolyPaint.Modeles.Strokes
+{
+    class AnchorPoint : CustomStroke
+    {
+        public readonly string ParentId;
+
+        public AnchorPoint(StylusPointCollection pts, CustomStrokeCollection strokes, string parentId) : base(pts, strokes)
+        {
+            this.ParentId = parentId;
+        }
+
+        public override void addDragHandles()
+        {
+            // An Anchor Point does not have handles
+        }
+
+        public override void deleteDragHandles()
+        {
+            // An Anchor Point does not have handles
+        }
+
+        public override StrokeType getType()
+        {
+            return StrokeType.ANCHOR_POINT;
+        }
+
+        public override bool HitTest(Point point)
+        {
+            return 6 > Math.Sqrt(Math.Pow(point.X - this.StylusPoints[0].X, 2) + Math.Pow(point.Y - this.StylusPoints[0].Y, 2));
+        }
+
+        public override bool isSelectable()
+        {
+            return false;
+        }
+
+        protected override void DrawCore(DrawingContext drawingContext, DrawingAttributes drawingAttributes)
+        {
+            Pen pen = new Pen(new SolidColorBrush(Colors.Gray), 1.5);
+            drawingContext.DrawEllipse(null, pen, this.StylusPoints[0].ToPoint(), 3, 3);
+
+        }
+    }
+}
