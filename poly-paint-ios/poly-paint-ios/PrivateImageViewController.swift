a92346a950a681b0736da19c6aa383a1af3e157c
Jean Paul Cech <jean-paul.cech@polymtl.ca>
2018-10-31 12:55:33 -0400

    Now possible to set image as protected

---
+++ poly-paint-ios/poly-paint-ios/PrivateImageViewController.swift
@@ -0,0 +1,112 @@
+//
+//  PrivateImageViewController.swift
+//  poly-paint-ios
+//
+//  Created by JP Cech on 10/28/18.
+//  Copyright © 2018 PolyAcme. All rights reserved.
+//
+
+import UIKit
+
+class PrivateImageViewController: UIViewController {
+    var image: Image?
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageProtectionLevelLabel: UILabel!
+    @IBOutlet weak var editBtn: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image?.fullImage
+        imageProtectionLevelLabel.text = image?.protectionLevel
+        self.navigationItem.title = image?.title
+    }
+    
+    @IBAction func editImage(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let openEditorAction = UIAlertAction(title: "Open In Editor", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+            //  Do some destructive action here.
+        })
+        alertController.addAction(openEditorAction)
+        
+        if image?.protectionLevel != "public" {
+            let makePublicAction = UIAlertAction(title: "Set As Public", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+                //  Do some action here.
+            })
+            alertController.addAction(makePublicAction)
+        }
+        
+        if image?.protectionLevel != "private" {
+            let makePrivateAction = UIAlertAction(title: "Set As Private", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+                //  Do some destructive action here.
+            })
+            alertController.addAction(makePrivateAction)
+        }
+        
+        if image?.protectionLevel != "protected" {
+            let makeProtectedAction = UIAlertAction(title: "Set As Protective", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+                self.performSegue(withIdentifier: "toChangeImagePassword", sender: self)
+            })
+            alertController.addAction(makeProtectedAction)
+        }
+        
+        if image?.protectionLevel == "protected" {
+            let changePasswordAction = UIAlertAction(title: "Change Password", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+                //  Do some destructive action here.
+            })
+            alertController.addAction(changePasswordAction)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: { (alert: UIAlertAction!) -> Void in
+            //  Do something here upon cancellation.
+        })
+        alertController.addAction(cancelAction)
+        
+        
+        
+        if let popoverController = alertController.popoverPresentationController {
+            popoverController.barButtonItem = sender
+        }
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toChangeImagePassword" {
+           let ProtectedImagePasswordVC = segue.destination as! ProtectedImagePasswordViewController
+            ProtectedImagePasswordVC.image = self.image
+        }
+    }
+    
+}
+
+/*if image?.protectionLevel != "public" {
+ let makePublicAction = UIAlertAction(title: "Public", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+ //  Do some action here.
+ })
+ alertController.addAction(makePublicAction)
+ }
+ if image?.protectionLevel != "private" {
+ let makePrivateAction = UIAlertAction(title: "Private", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+ //  Do some destructive action here.
+ })
+ alertController.addAction(makePrivateAction)
+ }
+ 
+ if image?.protectionLevel != "protected" {
+ let makeProtectedAction = UIAlertAction(title: "Protective", style: .default, handler: { (alert: UIAlertAction!) -> Void in
+ //  Do some destructive action here.
+ })
+ alertController.addAction(makeProtectedAction)
+ }
+ 
+ let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: { (alert: UIAlertAction!) -> Void in
+ //  Do something here upon cancellation.
+ })
+ alertController.addAction(cancelAction)*/
+
+/*if let popoverController = alertController.popoverPresentationController {
+ popoverController.sourceView = self.view
+ popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+ popoverController.permittedArrowDirections = []
+ }*/
---
+++ poly-paint-ios/poly-paint-ios/ProtectedImagePasswordViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ProtectedImagePasswordViewController.swift
+//  poly-paint-ios
+//
+//  Created by JP Cech on 10/30/18.
+//  Copyright © 2018 PolyAcme. All rights reserved.
+//
+
+import UIKit
+
+class ProtectedImagePasswordViewController: UIViewController {
+    var image: Image?
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var confirmNewPasswordTextField: UITextField!
+    @IBOutlet weak var passwordsDontMatchLabel: UILabel!
+    @IBOutlet weak var passwordChangedLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideLabels()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changePasswordTapped(_ sender: UIButton) {
+        let password = newPasswordTextField.text
+        let confirmPassword = confirmNewPasswordTextField.text
+        
+        if password != "" && password == confirmPassword {
+            self.changePassword(password: password!)
+        } else if password != confirmPassword {
+            self.passwordsDontMatchLabel.isHidden = false
+        }
+    }
+    
+    func changePassword(password: String) {
+        print("ello")
+        let urlString = "http://localhost:3000/v2/images/" + (image?.id)!
+        let url = URL(string: urlString)
+        let session = URLSession.shared
+        var request = URLRequest(url: url!)
+        request.httpMethod = "PUT"
+        let imageToSend: [String: Any] = [
+            "ownerId": (image?.ownerId)!,
+            "title" :(image?.title)!,
+            "protectionLevel": "protected",
+            "password": newPasswordTextField.text!,
+            "thumbnailUrl": "", // leave empty until thumbnails are supported. Would cause a nil crash while unwrapping otherwise
+            "fullImageUrl": (image?.fullImageUrl)!,
+            ]
+        let jsonData = try? JSONSerialization.data(withJSONObject: imageToSend, options: .prettyPrinted)
+    let jsonString = String(data: jsonData!, encoding: .utf8)
+        print(jsonString!)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            _ = response as? HTTPURLResponse
+            guard let data = data, error == nil else {
+                print("here")
+                return
+            }
+            
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            print(responseJSON!)
+            if (responseJSON as? [String: Any]) != nil {
+                print(responseJSON)
+                DispatchQueue.main.async {
+                    self.changePasswordDone()
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func changePasswordDone() {
+        self.resetPasswordLabelAndTextFields()
+        self.showPasswordChangedLabel()
+    }
+    
+    func showPasswordChangedLabel() {
+        self.passwordChangedLabel.isHidden = false
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            self.passwordChangedLabel.isHidden = true
+            self.passwordChangedLabel.alpha = 1
+        })
+    }
+    
+    func hideLabels(){
+        
+        self.passwordsDontMatchLabel.isHidden = true
+        self.passwordChangedLabel.isHidden = true
+    }
+    
+    func resetPasswordLabelAndTextFields() {
+        self.passwordsDontMatchLabel.isHidden = true
+        self.newPasswordTextField.text = ""
+        self.confirmNewPasswordTextField.text = ""
+    }
+    
+}
