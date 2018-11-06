7a64f6897be548a4cef6408d8e022078423380ae
Audrey Labrie <audrey.labrie@polymtl.ca>
2018-11-06 11:31:45 -0500

    added class diagram class

---
+++ poly-paint-ios/poly-paint-ios/New Group/ClassDiagramView.swift
@@ -0,0 +1,36 @@
+//
+//  ClassDiagramView.swift
+//  poly-paint-ios
+//
+//  Created by Tomato on 2018-11-04.
+//  Copyright © 2018 PolyAcme. All rights reserved.
+//
+
+import UIKit
+
+class ClassDiagramView: UIView {
+
+    let defaultWidth: CGFloat = 75
+    let defaultHeight: CGFloat = 100
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    init() {
+        let frame = CGRect(x: 0, y: 0, width: self.defaultWidth, height: self.defaultHeight)
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
