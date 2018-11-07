b9a2fee9e59f3050cc26f614e1169e3cc04dd545
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2018-11-07 11:24:09 -0500

    Added mocks of the objects that will be sent to the server

---
+++ windows-client/PolyPaint/Modeles/Strokes/SerializationHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PolyPaint.Modeles.Strokes
+{
+    public class SerializationHelper
+    {
+    }
+
+    public class SerializedStroke
+    {
+        public Guid Id;
+        public string Type;
+        public int Index;
+    }
+
+    public class SerializedShape : SerializedStroke
+    {
+        public Point Center;
+        public double Width;
+        public double Height;
+    }
+
+    public class SerializedTextableShape : SerializedShape
+    {
+        public List<string> Content;
+    }
+
+    public class SerializedLine : SerializedStroke
+    {
+        public List<Point> Points;
+
+        public string FirstAnchorId;
+        public int FirstAnchorIndex;
+        public string SecondAnchorId;
+        public int SecondAnchorIndex;
+
+        public string FirstEndLabel;
+        public string FirstEndRelation;
+        public string SecondEndLabel;
+        public string SecondEndRelation;
+    }
+}
