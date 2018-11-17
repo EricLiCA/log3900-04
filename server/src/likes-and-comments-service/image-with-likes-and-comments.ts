0f5511f195c9c9f1a19918cc91a7853e357635ec
Aymen <aymenamous@gmail.com>
2018-11-17 17:22:42 -0500

    Preparing socket fro dynamic likes and comments

---
+++ server/src/likes-and-comments-service/image-with-likes-and-comments.ts
@@ -0,0 +1,48 @@
+import { ImageComment } from "../models/image-comment";
+import { ImageLike } from "../models/image-like";
+
+export class ImageWithLikesAndComments {
+    public id: string;
+    public comments: ImageComment[];
+    public likes: ImageLike[];
+
+    constructor(id: string) {
+        this.id = id;
+    }
+
+    public loadComments(): Promise<ImageComment[]> {
+        return new Promise<ImageComment[]>((resolve, reject) => {
+            ImageComment.get(this.id).then(
+                (value: ImageComment[]) => {
+                    this.comments = value;
+                },
+                (rejectReason: any) => reject(rejectReason)
+            );
+        });
+    }
+
+    public loadLikes(): Promise<ImageLike[]> {
+        return new Promise<ImageLike[]>((resolve, reject) => {
+            ImageLike.get(this.id).then(
+                (value: ImageLike[]) => {
+                    this.likes = value;
+                },
+                (rejectReason: any) => reject(rejectReason)
+            );
+        });
+    }
+
+    public async addLike(userId: string): Promise<void> {
+        const imageLike = await ImageLike.post(new ImageLike(this.id, userId));
+        if (imageLike != undefined) {
+            this.likes.push(imageLike);
+        }
+    }
+
+    public async removeLike(userId: string): Promise<void> {
+        const imageLike = await ImageLike.delete(new ImageLike(this.id, userId));
+        if (imageLike != undefined) {
+            this.likes.splice(this.likes.findIndex(likeToRemove => likeToRemove.ImageId == imageLike.ImageId && likeToRemove.UserId == imageLike.UserId), 1);
+        }
+    }
+}
---
+++ server/src/likes-and-comments-service/likes-and-comments-service.ts
@@ -0,0 +1,75 @@
+import { ImageWithLikesAndComments } from "./image-with-likes-and-comments";
+import { User } from "../connected-users-service.ts/user";
+
+export class LikesAndCommentsService {
+    private static service: LikesAndCommentsService;
+    private images: ImageWithLikesAndComments[];
+    private users: Map<User, string>;
+
+    private constructor() {
+        this.images = [];
+        this.users = new Map<User, string>();
+    }
+
+    public static get instance(): LikesAndCommentsService {
+        if (LikesAndCommentsService.service === undefined) {
+            LikesAndCommentsService.service = new LikesAndCommentsService();
+        }
+        return this.service;
+    }
+
+    public getUserPreviwedImage(userSocketId: string): Promise<ImageWithLikesAndComments> {
+        let roomId: string = undefined;
+        this.users.forEach((id: string, user: User) => {
+            if (user.socket.id === userSocketId)
+                roomId = id;
+        });
+        return roomId == undefined ? undefined : this.getImage(roomId);
+    }
+
+    public async getImage(imageId: string): Promise<ImageWithLikesAndComments> {
+        let previewedImage = this.images.find((image: ImageWithLikesAndComments) => image.id === imageId);
+        if (previewedImage == undefined) {
+            previewedImage = new ImageWithLikesAndComments(imageId);
+            this.images.push(previewedImage);
+            await previewedImage.loadLikes();
+        }
+
+        return previewedImage;
+    }
+
+    public newConnection(user: User): void {
+
+        user.socket.on('previewImage', async (imageId: string) => {
+            const previousPreviewedImage = await this.getUserPreviwedImage(user.socket.id);
+            if (previousPreviewedImage != undefined) {
+                user.socket.leave(previousPreviewedImage.id);
+            }
+
+            this.users.set(user, imageId);
+            user.socket.join(imageId);
+        });
+
+        user.socket.on('leaveImage', async () => {
+            const previewedImage = await this.getUserPreviwedImage(user.socket.id);
+            user.socket.leave(previewedImage.id);
+            this.users.delete(user);
+        });
+
+        user.socket.on('addLike', async (userId: string) => {
+            const previewedImage = await this.getUserPreviwedImage(user.socket.id);
+            await previewedImage.addLike(userId);
+            user.socket.to(this.users.get(user)).emit('addLike', previewedImage.likes);
+        });
+
+        user.socket.on('removeLike', async (userId: string) => {
+            const previewedImage = await this.getUserPreviwedImage(user.socket.id);
+            await previewedImage.removeLike(userId);
+            user.socket.to(this.users.get(user)).emit('addLike', previewedImage.likes);
+        });
+    }
+
+    public async closeConnection(user: User) {
+        this.users.delete(user);
+    }
+}
