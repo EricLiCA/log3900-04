273710e72d4adb697b8c11bc212542f441e0e9eb
Eric <eric-li@hotmail.ca>
2018-11-19 17:35:39 -0500

    Added S3Service

---
+++ poly-paint-ios/poly-paint-ios/S3/S3Service.swift
@@ -0,0 +1,19 @@
+//
+//  S3Service.swift
+//  poly-paint-ios
+//
+//  Created by Eric Sida Li on 2018-11-16.
+//  Copyright © 2018 PolyAcme. All rights reserved.
+//
+
+import Foundation
+
+class S3Service {
+    static let instance = S3Service()
+    let accessKey = "AKIAJXZ5EJSDKT6KUYWQ"
+    let secretKey = "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"
+    
+    private init() {
+        
+    }
+}
