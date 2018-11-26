239b141d87c883d9cbdbc896d941a10735da6fa0
Aymen <aymenamous@gmail.com>
2018-11-26 15:23:02 -0500

    Canvas size exception

--- windows-client/PolyPaint/Modeles/Editeur.cs
+++ windows-client/PolyPaint/Modeles/Editeur.cs
@@ -533,7 +533,9 @@ namespace PolyPaint.Modeles
             {
                 Application.Current.Dispatcher.Invoke(() =>
                 {
-                    _CanvasSize = new Size(int.Parse(server_params[0].ToString()), int.Parse(server_params[1].ToString()));
+                    int x = int.Parse(server_params[0].ToString().Split(new char[] { ',', '.' })[0]);
+                    int y = int.Parse(server_params[1].ToString().Split(new char[] { ',', '.' })[0]);
+                    _CanvasSize = new Size(x, y);
                     ProprieteModifiee("CanvasSize");
                 });
             }));
